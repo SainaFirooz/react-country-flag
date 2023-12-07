e6e4aa69d2015e753b33f375fec3637361a062d3
Saina <saina.firooz@gmail.com>
2023-12-07 14:24:52 +0100

    continent compartment

---
+++ src/compartments/Continents.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Continents = ({ selectedContinent, onChange }) => {
+
+  return (
+    <select
+    id="continent"
+    value={selectedContinent}
+    onChange={(e) => onChange(e.target.value)}
+  >
+    <option value="asia">Asia</option>
+    <option value="africa">Africa</option>
+    <option value="europe">Europe</option>
+  </select>
+  );
+};
+
+export default Continents;
